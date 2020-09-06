d2ea151a183bc9a10cea4da12dec45da3a5ca76e
ali-linux <mynameisali1999@gmail.com>
2020-09-06 10:37:32 +0300

    local

--- btre/settings.py
+++ btre/settings.py
@@ -138,14 +138,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-#EMAIL CONFIG
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ali.srwsht.ali@gmail.com'
-EMAIL_HOST_PASSWORD = 'bbzmdnauazrwfrbk'
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
